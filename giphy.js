9da55d82dda4bdd3dd5ee6dc787d93b1c2ef10c9
David Li <jdavidli01@outlook.com>
2016-08-10 11:11:11 -0400

    fixed 2 bugs, app works

--- giphy.js
+++ giphy.js
@@ -1,10 +1,10 @@
 $(function() {
-	populate(animals, 'animalName', '.animalName');
+	populate(animals, 'animalName', '#animalName');
 });
 
 var animals = ["dog", "cat", "rabbit", "hamster", "skunk", 
 				"goldfish", "bird", "ferret", "turtle", 
-				"sugar glider", "chinchilla"]
+				"sugar glider", "chinchilla"];
 
 
 function populate(useArray, newClass, addArea){
@@ -26,10 +26,9 @@ $(document).on('click', '.animalName', function(){
 	$(this).addClass('active');
 
 	var type = $(this).data('type');
-	var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + 
-				type + "&api_key=dc6zaTOxFJmzC&limit=10";
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-	$.ajax({url: queryUrl, method: 'GET'})
+	$.ajax({url: queryURL, method: 'GET'})
 	.done(function(response) {
 		var results = response.data;
 
@@ -43,18 +42,20 @@ $(document).on('click', '.animalName', function(){
 			var p = $('<p>').text("Rating: " + rating);
 			//save url and still
 			var animated = results[i].images.fixed_height.url;
-			var still = results[i].images.fixed_still.url;
+            var still = results[i].images.fixed_height_still.url;
 			//create image tag
 			var image = $('<img>');
 			//include following attributes on the tag
 			image.attr('src', still);
 			image.attr('data-state', still);
+			image.attr('data-animate', animated);
+			image.attr('data-state', 'still')
 			image.addClass('animalImage');
 			//append p and image onto div
 			div.append(p);
 			div.append(image);
 			//append div to the html
-			$('gifAnimal').append(div);
+			$('.gifAnimal').append(div);
 		}
 	});
 });
@@ -64,5 +65,21 @@ $(document).on('click', '.image', function(){
 
 	if(state === 'still'){
 		$(this).attr('src', $(this).data('animate'));
+		$(this).attr('data-state', 'animated');
+	} else {
+		$(this).attr('src', $(this).data('still'));
+		$(this).attr('data-state', 'still');
 	}
 })
+
+$('.submit').on('click', function(){
+	var newA = $('input').eq(0).val();
+
+	if (newA.length > 2){
+		animals.push(newA);
+	}
+
+	populate(animals, 'animalName', '#animalName');
+
+	return false;
+});
